0c17711e11c5eda4eeec24e1d3246f6208396ca4
Matthew Coburn <mattcoburn24@gmail.com>
2020-12-07 21:29:28 -0800

    added a photoHandler to set state when a photo is selected, outputs an error if the wrong file type is selected

---
+++ src/comps/UploadForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+export const UploadForm = () => {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
+  const photoHandler = (e) => {
+    let selected = e.target.files[0];
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+    } else {
+      setFile(null);
+      setError("Please select an image file (png or jpeg)");
+    }
+  };
+
+  return (
+    <form>
+      <input onChange={photoHandler} type="file"></input>
+      <div className="output">
+        {error && <div className="error">{error}</div>}
+      </div>
+    </form>
+  );
+};
+
+export default UploadForm;
