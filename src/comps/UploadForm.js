03e69f0e43363062cd8702006e6a0fd9baafdba9
Matthew Coburn <mattcoburn24@gmail.com>
2020-12-07 22:20:01 -0800

    created custom useStorage hook to set a Ref for the image file and then upload it. Returns a progress percentage, url, and error if there is one

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -10,6 +10,7 @@ export const UploadForm = () => {
     let selected = e.target.files[0];
     if (selected && types.includes(selected.type)) {
       setFile(selected);
+      setError("");
     } else {
       setFile(null);
       setError("Please select an image file (png or jpeg)");
@@ -21,6 +22,7 @@ export const UploadForm = () => {
       <input onChange={photoHandler} type="file"></input>
       <div className="output">
         {error && <div className="error">{error}</div>}
+        {file && <div>{file.name}</div>}
       </div>
     </form>
   );
